35d7cd88aea5daf1b95258abec7a72fbe49aed54
Gigatrappeur <gigatrappeur@gmail.com>
2013-08-13 18:42:03 +0200

    Création de la solution
    Ajout du projet NLE (contient les classes pour la DLL)
    Ajout du projet Test (contient le code pour tester la DLL)

---
+++ NLE/TestClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NLE
+{
+    public class TestClass
+    {
+
+        public TestClass()
+        { }
+
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            NLE.TestClass t = new NLE.TestClass();
+            int r = t.add(1, 2);
+
+            Console.WriteLine("Résultat : " + r);
+
+            Console.Write("Appuyer sur une touche...");
+            Console.ReadKey();
+        }
+    }
+}
